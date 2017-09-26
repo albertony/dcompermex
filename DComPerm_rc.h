6880612b85e24961e3b71cc69fc94d77e82d7600
albertony <12441419+albertony@users.noreply.github.com>
2017-09-26 22:31:14 +0200

    Version number 2.0.0.0

--- DComPerm_rc.h
+++ DComPerm_rc.h
@@ -5,9 +5,9 @@
 #define APPLICATION_FILENAME APPLICATION_NAME ".exe"
 #define APPLICATION_DESCRIPTION "DCOM permission command line utility"
 
-#define VERSION_MAJOR 0
-#define VERSION_MINOR 1
-#define VERSION_PATCH 2
+#define VERSION_MAJOR 2
+#define VERSION_MINOR 0
+#define VERSION_PATCH 0
 #define VERSION_BUILD 0
 
 #define STRINGIFY_INDIRECT(x) #x
